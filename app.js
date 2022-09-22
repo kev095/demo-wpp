a51842229653df6561540ea11bf4c1851a8a2c86
KevinJoseph <kevramos95@gmail.com>
2022-09-22 17:46:58 -0500

    modify

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ app.post("/webhook", (req, res) => {
       let phone_number_id =
         req.body.entry[0].changes[0].value.metadata.phone_number_id;
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      //let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      let msg_body = 'Hola, estamos en desarrollo.'
       axios({
         method: "POST", // Required, HTTP method, a string, e.g. POST, GET
         url:
