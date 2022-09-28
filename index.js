3f910948718718aecaf9fc2ce7c264f7f59fc2ff
KevinJoseph <kevramos95@gmail.com>
2022-09-28 12:22:05 -0500

    dasd

--- index.js
+++ index.js
@@ -24,31 +24,6 @@ app.post("/webhook", (req, res) => {
 
 
 
-  //send email
-  const nodemailer = require("nodemailer"); // Require the Nodemailer package
-async function main() {
-  // SMTP config
-  const transporter = nodemailer.createTransport({
-    host: "mail.1bconnect.com", //
-    port: 465,
-    auth: {
-      user: "kevin@1bconnect.com", // Your Ethereal Email address
-      pass: "Pp131471483@", // Your Ethereal Email password
-    },
-  }); // Send the email
-  let info = await transporter.sendMail({
-    from: '"Kevin Ramos" <kevin@1bconnect.com>',
-    to: "kevramos95@gmail.com", // Test email address
-    subject: "I love SMTP!",
-    text: "Here's a text version of the email.",
-    html: "Here's an <strong>HTML version</strong> of the email.",
-  });
-  console.log("Message sent: %s", info.messageId); // Output message ID
-  console.log("View email: %s", nodemailer.getTestMessageUrl(info)); // URL to preview email
-}
-// Catch any errors and output them to the console
-main().catch(console.error);
-
 
 
 
