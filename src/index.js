1b139be4aed5c2a4c762e35342d696d5836747f5
KevinJoseph <kevramos95@gmail.com>
2022-10-21 00:42:16 -0500

    new

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ app.post("/webhook", (req, res) => {
         const data = {
           messaging_product: "whatsapp",
           to: from,
-          text: { body: "Ack: " + msg_body + JSON.stringify(client.data)  },
+          text: { body: "Hola: " +  JSON.stringify(client.data.name) + ", tienes una deuda de S/100.00"  },
         }
 
         axios.post("https://graph.facebook.com/v12.0/" + phone_number_id + "/messages?access_token=" +token, data,{headers: { "Content-Type": "application/json" }})
