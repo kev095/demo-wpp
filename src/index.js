55f07098d4baeed0ae8b4db38c826863c802d54b
KevinJoseph <kevramos95@gmail.com>
2022-10-21 13:08:08 -0500

    demo

--- src/index.js
+++ src/index.js
@@ -92,11 +92,25 @@ app.post("/webhook", (req, res) => {
       data: {
         messaging_product: "whatsapp",
         to: from,
-        text: { body:  msg_body },
+        text: { body:  "Hola, bienvenido" },
       },
       headers: { "Content-Type": "application/json" },
     });
 
+    axios({
+      method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+      url:
+        "https://graph.facebook.com/v12.0/" +
+        phone_number_id +
+        "/messages?access_token=" +
+        token,
+      data: {
+        messaging_product: "whatsapp",
+        to: from,
+        text: { body:  dataInitial },
+      },
+      headers: { "Content-Type": "application/json" },
+    });
 
 
 
