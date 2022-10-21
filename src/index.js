4dbdb5e70c543d9b664b468f07a5ba0161c3463d
KevinJoseph <kevramos95@gmail.com>
2022-10-21 12:36:22 -0500

    list options wpp

--- src/index.js
+++ src/index.js
@@ -46,7 +46,64 @@ app.post("/webhook", (req, res) => {
           text: { body: "Hola " +  result.data.name +" "+ result.data.lastname + " tienes una deuda de S/100.00" },
         }
 
-        axios.post("https://graph.facebook.com/v12.0/" + phone_number_id + "/messages?access_token=" +token, data,{headers: { "Content-Type": "application/json" }})
+        const data2 ={
+          
+            messaging_product: "whatsapp",
+            recipient_type: "individual",
+            to: from,
+            type: "interactive",
+            interactive: {
+              type: "list",
+              header: {
+                type: "text",
+                text: "HEADER_TEXT"
+              },
+              body: {
+                text: "BODY_TEXT"
+              },
+              footer: {
+                text: "FOOTER_TEXT"
+              },
+              action: {
+                button: "BUTTON_TEXT",
+                sections: [
+                  {
+                    title: "SECTION_1_TITLE",
+                    rows: [
+                      {
+                        id: "SECTION_1_ROW_1_ID",
+                        title: "SECTION_1_ROW_1_TITLE",
+                        description: "SECTION_1_ROW_1_DESCRIPTION"
+                      },
+                      {
+                        id: "SECTION_1_ROW_2_ID",
+                        title: "SECTION_1_ROW_2_TITLE",
+                        description: "SECTION_1_ROW_2_DESCRIPTION"
+                      }
+                    ]
+                  },
+                  {
+                    title: "SECTION_2_TITLE",
+                    rows: [
+                      {
+                        id: "SECTION_2_ROW_1_ID",
+                        title: "SECTION_2_ROW_1_TITLE",
+                        description: "SECTION_2_ROW_1_DESCRIPTION"
+                      },
+                      {
+                        id: "SECTION_2_ROW_2_ID",
+                        title: "SECTION_2_ROW_2_TITLE",
+                        description: "SECTION_2_ROW_2_DESCRIPTION"
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          
+        }
+
+        axios.post("https://graph.facebook.com/v12.0/" + phone_number_id + "/messages?access_token=" +token, data2,{headers: { "Content-Type": "application/json" }})
     }).catch(err => {
 
 
